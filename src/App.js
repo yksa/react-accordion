7dd0a9215aeac9edd0a943a39b89e6c9064e6138
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-09 14:39:27 +0630

    react accordion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <h2>questions and answers about login</h2>
+        <section className="info">
+          {
+            questions.map(question => <SingleQuestion key={question.id} {...question} />)
+          }
+        </section>
+      </div>
+    </main>
   );
 }
 
--- src/Question.js
+++ src/Question.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-function Question() {
+function Question({ title, info }) {
+    const [showInfo, setShowInfo] = useState(false);
     return (
-        <div>
-            <h2>question component</h2>
-        </div>
+        <article className="question">
+            <header>
+                <h4>{title}</h4>
+                <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
+            </header>
+            {
+                showInfo && <p>{info}</p>
+            }
+        </article>
     )
 }
 
