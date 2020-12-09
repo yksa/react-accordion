4af8ec54daa0b7f5905954c27d31aca7371531f7
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-09 14:22:42 +0630

    Initialize project using Create React App

---
+++ src/Question.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+function Question() {
+    return (
+        <div>
+            <h2>question component</h2>
+        </div>
+    )
+}
+
+export default Question
